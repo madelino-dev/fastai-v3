daa50398d63e36ab6980de3c2608a19c9c10e7d4
madelino-dev <55136046+madelino-dev@users.noreply.github.com>
2019-09-13 04:17:41 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/tgtzvlm3ezoy6wc/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/tgtzvlm3ezoy6wc/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['bluegill', 'crappie', 'largemouth', 'perch', 'smallmouth']
